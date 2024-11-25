9debe663d53ba6ddc0be4ed64a10896797a4b003
Sienna White <siennaw@berkeley.edu>
2024-11-25 10:07:24 -0800

    fixed postprocessor.

---
+++ set_up_WPS/2_move_processed_files.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Where the processed grib / netcdf files live 
+SOURCE_DIR='/global/scratch/users/siennaw/gsi_2024/grib2nc/working/'
+
+# Where we want to move all the finished files 
+DEST_DIR='/global/scratch/users/siennaw/gsi_2024/grib2nc/finished/'
+
+# Ensure the destination directory exists
+#mkdir -p "$DEST_DIR"
+
+# Loop through each folder in the source directory
+for folder in "$SOURCE_DIR"/*; do
+    if [ -d "$folder" ]; then
+        # Find and move files starting with 'wrfinput_' to the destination directory
+        # thank you chatgpt for this line of code
+        find "$folder" -maxdepth 1 -type f -name "wrfinput_*" -exec mv {} "$DEST_DIR" \;
+    fi
+done 
